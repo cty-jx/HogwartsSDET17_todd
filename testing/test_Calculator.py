9ed208f016e64f27bf9b01279463796f8c141181
toddchen <toddchen@xiaoe-tech.com>
2021-01-29 12:42:31 +0800

    作业【相加】与【相除】

--- testing/test_Calculator.py
+++ testing/test_Calculator.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import sys
-
 import pytest
 import yaml
-
-sys.path.append('..')
-print(sys.path)
 from pythoncode.Calculator import Calculator
 
 
 def get_datas():
     with open("./datas/calc.yml") as f:
         datas = yaml.safe_load(f)
-    return (datas['add']['datas'], datas['add']['ids'])
+    # return (datas['add']['datas'], datas['add']['ids'])
+    return datas
 
 
 # yaml json excel csv xml
@@ -31,18 +26,42 @@ class TestCalc:
     def teardown_class(self):
         print("结束计算")
 
-    @pytest.mark.parametrize("a, b, result", datas[0], ids=datas[1])
-    def test_add(self, a, b, result):
-        print(f"a={a} , b ={b} ,result={result}")
-        assert result == self.calc.add(a, b)
+    @pytest.mark.run(order=2)
+    @pytest.mark.parametrize('a,b,expect', get_datas()['add']['datas'], ids=get_datas()['add']['ids'])
+    def test_add(self, a, b, expect):
+        result = self.calc.add(a,b)
+        assert expect == result
 
     def test_add1(self):
         datas = [[1, 1, 2], [100, 400, 300], [1, 0, 1]]
         for data in datas:
-            print(data)
+            # print(data)
             assert data[2] == self.calc.add(data[0], data[1])
 
-    # TODO: 完善相加功能
-    # TODO: 相除功能
-    def test_div(self):
-        pass
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize('a,b,expect', get_datas()['div']['datas'], ids=get_datas()['div']['ids'])
+    def test_div(self, a, b, expect):
+        if b == 0:
+            try:
+                self.calc.div(a, b)
+            except ZeroDivisionError as e:
+                print("除数不能为0")
+        else:
+            result = self.calc.div(a, b)
+            assert result == expect
+
+    def test_div1(self):
+        datas = [[1, 1, 1], [100, 400, 0.25], [1, 0, 1]]
+        for data in datas:
+            if data[1] == 0:
+                try:
+                    self.calc.div(data[0],data[1])
+                except ZeroDivisionError as e:
+                    print("除数不能为0")
+            else:
+                result = self.calc.div(data[0],data[1])
+                assert result == data[2]
+
+
+if __name__ == '__main__':
+    pytest.main(["test_Calculator.py::TestCalc","-s"])
