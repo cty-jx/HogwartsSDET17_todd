a2494ad708ff1fa60c3529b2774a4e4946a6e52d
toddchen <toddchen@xiaoe-tech.com>
2021-02-26 10:34:17 +0800

    selenium学习

--- web/selenium_windows/Base.py
+++ web/selenium_windows/Base.py
@@ -1,11 +1,18 @@
 #!usr/bin/env python
 # -*- coding:utf-8 -*-
+import os
 
 from selenium import webdriver
 
 class Base():
     def setup(self):
-        self.driver = webdriver.Chrome()
+        browser = os.getenv("browser")
+        if browser == 'firefox':
+            self.driver = webdriver.Firefox()
+        elif browser == 'headless':
+            self.driver = webdriver.PhantomJS()
+        else:
+            self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(5)
         self.driver.maximize_window()
 
--- web/selenium_windows/test_frame.py
+++ web/selenium_windows/test_frame.py
@@ -7,5 +7,9 @@ class TestFrame(Base):
 
     def test_frame(self):
         self.driver.get("https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
-        self.driver.switch_to_window("iframeResult")
+        # self.driver.switch_to_window("iframeResult")
+        self.driver.switch_to.frame("iframeResult")
         print(self.driver.find_element_by_id("draggable").text)
+        # self.driver.switch_to.parent_frame()
+        self.driver.switch_to.default_content()
+        print(self.driver.find_element_by_id("submitBTN").text)
